5219fcfcd85759aeaafab4cf6c64b796d147a13b
williangaspar <williangaspar360@gmail.com>
2018-12-15 09:58:21 -0200

    python removed

--- autocomplete/auto.sh
+++ autocomplete/auto.sh
@@ -1,15 +1,32 @@
 #/usr/bin/env bash
 
-# copy auto.sh and find.py to the bpftrace's executable folder and run 'source auto.sh' to activate
+# copy this file to the bpftrace's executable folder and run 'source auto.sh' to activate
 
 _words_complete()
 {
-    local first second opts
+    local cur prev result opts
     COMPREPLY=()
-    first="${COMP_WORDS[COMP_CWORD]}"
-    second="${COMP_WORDS[COMP_CWORD-1]}"
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts="-l -e -p -v -d -dd"
+    probes="kprobe kretprobe uprobe uretprobe tracepoint ustd profile interval software hardware"
 
-    COMPREPLY=( $(sudo python find.py ${first}) )
-    return 0
+    local no_quote_cur=$(echo "${cur}" | sed 's/\x27//g')
+
+    if [[ ${cur} == -* ]] ; then
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+        return 0
+    elif [[ ${cur} =~ kprobe* ]] ; then
+        local result=$(sudo bpftrace -l | grep "${no_quote_cur}")
+        COMPREPLY=( $(compgen -W "${result}" -- ${cur}) )
+        return 0
+    elif [[ ${cur} =~ tracepoint* ]] ; then
+        local result=$(sudo bpftrace -l "tracepoint:*" | grep "${no_quote_cur}")
+        COMPREPLY=( $(compgen -W "${result}" -- ${cur}) )
+        return 0
+    else
+        COMPREPLY=( $(compgen -W "${probes}" -- ${cur}) )
+        return 0
+    fi
 }
 complete -o nospace -o noquote -F _words_complete bpftrace
